997094b973552c73903508a292a48b43a57728bf
Edemone <edem.agbenyo@gmail.com>
2020-03-10 20:52:59 +0000

    print weather information on log

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css'
+window.onload = function(){
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=f06f39597935c040f0dde71ac7cfd97c',{
+  mode:'cors'
+})
+.then((response)=>{
+  return response.json();
+})
+.then(response=>console.log(response))
+.catch(error=>{
+  console.log(error);
+});
+}
