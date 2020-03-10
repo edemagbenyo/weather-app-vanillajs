be628afddc553622fd75b0041e6d8842a71a3de5
Edemone <edem.agbenyo@gmail.com>
2020-03-10 01:40:57 +0000

    add loaders  dependencies to webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.export={
+  entry:'./src/index.js',
+  output:{
+    filename:'main.js',
+    path:path.join(__dirname,'dist')
+  },
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use:[
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test:/\.(png|gif|jpe?g)$/,
+        use:'file-loader'
+      }
+    ]
+  }
+}
